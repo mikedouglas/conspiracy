e7a3e97fa092028233afc9415c645b0a1c79ce63
Mike Douglas <git@entropy.io>
2011-08-03 12:40:39 -0700

    Extract out evalEqual pattern.

--- test/eval.js
+++ test/eval.js
@@ -11,11 +11,15 @@ e.special_form('define', function (_, sym, val) {
   e.define(sym.name, e.eval(val));
 });
 
+assert.evalEqual = function (str, expected) {
+  assert.equal(e.evalAll(reader.parse(str)), expected);
+};
+
 vows.describe("EVAL").addBatch({
   "should find pre-defined symbols": function () {
     assert.equal(e.eval({type: 'id', name: 'sym'}), 'val');
     assert.equal(e.evalAll([{type: 'id', name: 'sym'}]), 'val');
-    assert.equal(e.evalAll(reader.parse('sym')), 'val');
+    assert.evalEqual('sym', 'val');
   },
 
   "should find dynamically-defined symbols": function () {
@@ -23,34 +27,31 @@ vows.describe("EVAL").addBatch({
   },
 
   "should be able to apply builtin functions": function () {
-    assert.equal(e.evalAll(reader.parse('(two)')), 2);
-    assert.equal(e.evalAll(reader.parse('(double 5)')), 10);
+    assert.evalEqual('(two)', 2);
+    assert.evalEqual('(double 5)', 10);
   },
 
   "should be able to apply lambdas": function () {
     assert.equal(typeof e.evalAll(reader.parse('(lambda (x) x)')), 'function');
     assert.equal(e.evalAll(reader.parse('(lambda (x) x)')).apply(undefined, [{}, 5]), 5);
 
-    var val = "((lambda (x) x) 5)";
-    //assert.equal(e.evalAll(reader.parse(val)), 5);
+    assert.evalEqual('((lambda (x) x) 5)', 5);
 
     // yo dawg...
-    var val2 = '((lambda (x) ((lambda (x) x) x)) 5)';
-    //assert.equal(e.evalAll(reader.parse(val2)), 5);
+    assert.evalEqual('((lambda (x) ((lambda (x) x) x)) 5)', 5);
   },
 
   "should be able to define symbols": function () {
-    assert.equal(e.evalAll(reader.parse('(define answer 42) answer')), 42);
-    assert.equal(e.evalAll(reader.parse('(define id (lambda (x) x)) (id 5)')), 5);
+    assert.evalEqual('(define answer 42) answer', 42);
+    assert.evalEqual('(define id (lambda (x) x)) (id 5)', 5);
   },
 
   "should scope properly": function () {
     // shadow variables
-    var val = "(define x 6) ((lambda (x) x) 5)";
-    assert.equal(e.evalAll(reader.parse(val)), 5);
+    assert.evalEqual("(define x 6) ((lambda (x) x) 5)", 5);
 
     // lambda does not use dynamic scoping
-    var val2 = "(define x 5) (define f (lambda () x)) ((lambda (x) (f)) 6)";
-    assert.equal(e.evalAll(reader.parse(val2)), 5);
+    val2 = "(define x 5) (define f (lambda () x)) ((lambda (x) (f)) 6)";
+    assert.evalEqual(val2, 5);
   }
 }).export(module);
