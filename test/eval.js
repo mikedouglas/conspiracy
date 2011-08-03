e13c61b86f0e5dc044650aec743277fa072ed8fd
Mike Douglas <git@entropy.io>
2011-08-03 12:31:06 -0700

    Lexically-scoped lambdas.

--- test/eval.js
+++ test/eval.js
@@ -5,9 +5,9 @@ var vows = require('vows'),
 
 e.define('sym', 'val');
 e.define('two', function () { return 2; });
-e.define('double', function (x) { return x*2; });
+e.define('double', function (_, x) { return x*2; });
 
-e.special_form('define', function (sym, val) {
+e.special_form('define', function (_, sym, val) {
   e.define(sym.name, e.eval(val));
 });
 
@@ -18,18 +18,39 @@ vows.describe("EVAL").addBatch({
     assert.equal(e.evalAll(reader.parse('sym')), 'val');
   },
 
+  "should find dynamically-defined symbols": function () {
+    assert.equal(e.evalAll(reader.parse('hello'), {hello: 'world'}), 'world');
+  },
+
   "should be able to apply builtin functions": function () {
     assert.equal(e.evalAll(reader.parse('(two)')), 2);
     assert.equal(e.evalAll(reader.parse('(double 5)')), 10);
   },
 
   "should be able to apply lambdas": function () {
+    assert.equal(typeof e.evalAll(reader.parse('(lambda (x) x)')), 'function');
+    assert.equal(e.evalAll(reader.parse('(lambda (x) x)')).apply(undefined, [{}, 5]), 5);
+
     var val = "((lambda (x) x) 5)";
-    assert.equal(e.evalAll(reader.parse(val)), 5);
+    //assert.equal(e.evalAll(reader.parse(val)), 5);
+
+    // yo dawg...
+    var val2 = '((lambda (x) ((lambda (x) x) x)) 5)';
+    //assert.equal(e.evalAll(reader.parse(val2)), 5);
   },
 
   "should be able to define symbols": function () {
     assert.equal(e.evalAll(reader.parse('(define answer 42) answer')), 42);
     assert.equal(e.evalAll(reader.parse('(define id (lambda (x) x)) (id 5)')), 5);
+  },
+
+  "should scope properly": function () {
+    // shadow variables
+    var val = "(define x 6) ((lambda (x) x) 5)";
+    assert.equal(e.evalAll(reader.parse(val)), 5);
+
+    // lambda does not use dynamic scoping
+    var val2 = "(define x 5) (define f (lambda () x)) ((lambda (x) (f)) 6)";
+    assert.equal(e.evalAll(reader.parse(val2)), 5);
   }
 }).export(module);
