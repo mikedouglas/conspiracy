39ae2cbd4d70892d423f5aaf5b4ef1534f5302a4
Mike Douglas <git@entropy.io>
2011-08-05 11:36:51 -0700

    Explicit TCO with `recur`.

---
+++ test/lambda.js
@@ -0,0 +1,25 @@
+var vows = require('vows'),
+  assert = require('assert'),
+       e = require('../lib/eval'),
+       r = require('../lib/reader');
+
+require('../lib/lambda');
+require('../lib/primitives');
+
+assert.evalEqual = function (str, expected) {
+  assert.equal(e.evalAll(r.parse(str)), expected);
+};
+
+assert.evalThrows = function (str) {
+  assert.throws(function () {
+    e.evalAll(r.parse(str));
+  });
+};
+
+vows.describe("Lambda").addBatch({
+  "should optimize tail recursion when explicitly told to": function () {
+    assert.evalEqual("((lambda (n a b) (if (eq? n 0) b (recur (- n 1) b (+ a b)))) 2000 0 1)", Infinity);
+    assert.evalEqual("(define (f n a b) (if (eq? n 0) b (recur (- n 1) b (+ a b)))) (f 2000 0 1)", Infinity);
+    assert.evalThrows("(define (f n a b) (if (eq? n 0) b (f (- n 1) b (+ a b)))) (f 2000 0 1)");
+  }
+}).export(module);
