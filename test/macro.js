f9cf94be4b41c9ba151809b553108cc7adabd52f
Mike Douglas <git@entropy.io>
2011-08-04 20:37:27 -0700

    Macros now dynamically eval after execution.

--- test/macro.js
+++ test/macro.js
@@ -17,6 +17,14 @@ assert.evalOk = function (str, expected) {
 
 vows.describe("Macro System").addBatch({
   "should not eval arguments": function () {
-    assert.evalOk("(eq? ((macro (x) x) hello) 'hello)");
+    assert.evalOk("(eq? ((macro (x) (if (eq? x 'hello) 5)) hello) 5)");
+  },
+
+  "should eval results": function () {
+    assert.evalEqual("((macro () '(+ 3 4)))", 7);
+    assert.evalEqual("(let ((f (macro (x) " +
+                                "(if (eq? x 'foo) 100 " +
+                                "(if (eq? x 'bar) 200))))) " +
+                       "(+ (f foo) (f bar)))", 300);
   }
 }).export(module);
