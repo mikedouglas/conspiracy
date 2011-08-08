37fa3f1837678b0a5e15b5388ff24839a81e0a9d
Mike Douglas <git@entropy.io>
2011-08-08 11:38:49 -0700

    Add basic arity checking to function calls.

--- test/lambda.js
+++ test/lambda.js
@@ -21,5 +21,9 @@ vows.describe("Lambda").addBatch({
     assert.evalEqual("((lambda (n a b) (if (eq? n 0) b (recur (- n 1) b (+ a b)))) 2000 0 1)", Infinity);
     assert.evalEqual("(define (f n a b) (if (eq? n 0) b (recur (- n 1) b (+ a b)))) (f 2000 0 1)", Infinity);
     assert.evalThrows("(define (f n a b) (if (eq? n 0) b (f (- n 1) b (+ a b)))) (f 2000 0 1)");
+  },
+
+  "should throw if arity mismatch": function () {
+    assert.evalThrows("((lambda (x) x))");
   }
 }).export(module);
