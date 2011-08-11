e7a39b26a43cd96af5ef1c64fb55b15bbb062d0b
Mike Douglas <git@entropy.io>
2011-08-10 17:11:11 -0700

    Add /examples folder to static server.

--- web/repl.js
+++ web/repl.js
@@ -3,6 +3,7 @@ var server = connect.createServer();
 
 server.use(connect.static(__dirname));
 server.use('/lib', connect.static(__dirname + '/../lib'));
+server.use('/examples', connect.static(__dirname + '/../examples'));
 server.use(require('browserify')({
   require: [__dirname + '/../lib/eval.js',
             __dirname + '/../lib/lambda.js',
